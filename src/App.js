ca0bf9d80e1220655a48eda25325887ff98b18fd
Sialsanwal <56753162+Sialsanwal@users.noreply.github.com>
2020-11-14 12:04:10 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Paper,TextField} from '@material-ui/core/';
+import { colors } from '@material-ui/core';
+import { blue } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: "lightBlue",
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+  paper: {
+    height: 500,
+    width: 400,
+    margin: "0 auto",
+    textAlign: "center",
+  }
+}));
+
+
+
+
+
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+
+      <Paper elevation={3} className={classes.paper}>
+        <h1> Login</h1>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField  label="Enter Email" />
+          
+          <TextField  label="Enter Password" />
+
+        
+        </form>
+      </Paper>
     </div>
   );
 }
