826f47b8e9a030b42bbe0cbebf6c5bda152ac0ff
Sialsanwal <56753162+Sialsanwal@users.noreply.github.com>
2020-11-14 12:26:51 +0500

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {Paper,TextField} from '@material-ui/core/';
+import { Paper, TextField, Button} from '@material-ui/core/';
+import Navbar from './nav';
 import { colors } from '@material-ui/core';
 import { blue } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: "lightBlue",
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
-    },
-  },
+  
   paper: {
-    height: 500,
+    
     width: 400,
     margin: "0 auto",
     textAlign: "center",
+    padding:15
+  },
+  input:{
+    width:"95%",
+    marginBottom:10
   }
 }));
 
@@ -34,15 +30,17 @@ function App() {
 
   return (
     <div className={classes.root}>
-
+        <Navbar/>
       <Paper elevation={3} className={classes.paper}>
         <h1> Login</h1>
         <form className={classes.root} noValidate autoComplete="off">
-          <TextField  label="Enter Email" />
-          
-          <TextField  label="Enter Password" />
+          <TextField label="Enter Email" className={classes.input} />
+          <TextField label="Enter Password" className={classes.input} />
+          <Button variant="contained" color="primary" className={classes.input}>
+            Login
+          </Button>
 
-        
+
         </form>
       </Paper>
     </div>
